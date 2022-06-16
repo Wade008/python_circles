36521244f64179a93d2ce28550137ec69ed9e853
Wade Doolan <wadedoolan@hotmail.com>
2022-06-16 11:34:15 +1000

    completed program

---
+++ circles.py
@@ -0,0 +1,26 @@
+# calcuate area and circumference of a circle based on user input
+
+# Set base variables
+welcome = "Welcome to circles.py!\nThis program will calculate the area and circumference of a circle."
+
+radPrompt = "\n\nPlease enter the radius of the circle:  "
+
+pi = 3.141529
+
+# print welcome message
+print(welcome)
+
+# user input prompt
+response = input(radPrompt)
+
+# convert str to float
+radius = float(response)
+
+#Calculate circumference and area based on input
+circumference = 2 * pi * radius
+area = pi * (radius ** 2)
+
+print(f"\nCircumference: {circumference}")
+print(f"Area: {area}")
+
+
